ede3fa56da8ae6a65ecc95d7a156820aed7dd31b
Tim Hilt <timhilt@live.de>
2020-10-16 14:07:59 +0200

    Deleted requirements.txt

--- setup.py
+++ setup.py
@@ -5,8 +5,13 @@ setuptools.setup(
     version='0.0.1',
     packages=setuptools.find_packages(),
     url='',
-    license='mit',
+    license='MIT',
     author='Tim Hilt',
     author_email='timhilt@live.de',
-    description='Implementation of Vector of Locally Aggregated Descriptors (VLAD) Proposed by Jégou et al'
+    description='Implementation of Vector of Locally Aggregated Descriptors (VLAD) Proposed by Jégou et al',
+    install_requires=[
+        "numpy",
+        "scikit-learn",
+        "progressbar2"
+    ]
 )
