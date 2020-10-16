918fbc316ca5a71899aeba724abb237af87d4adb
Tim Hilt <timhilt@live.de>
2020-10-16 11:45:32 +0200

    Adding README.md and LICENSE

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='vlad',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='mit',
+    author='Tim Hilt',
+    author_email='timhilt@live.de',
+    description='Implementation of Vector of Locally Aggregated Descriptors (VLAD) Proposed by Jégou et al'
+)
